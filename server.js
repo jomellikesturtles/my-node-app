b646f0c926b840dc40e271766bca25e2038a23f8
jomellikesturtles <jommelsaligumba@gmail.com>
2020-04-13 19:51:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+app.get('/stream', function (req, res) {
+  res.send('stream page works!');
+})
+app.get('/addition', function (req, res) {
+  let num1 = req.query.num1;
+  let num2 = req.query.num2;
+  res.send('answer: ' + (num1+num2));
+})
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
